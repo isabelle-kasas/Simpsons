dea543417910afc4a4d72b8d17363b265ff54ddf
isabelle kasas <isabellekasas@mbp-de-isabelle.home>
2020-10-07 10:25:28 +0200

    ajout de la méthode map dans ContactList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
-
+import Navbar from './component/Navbar'
+import QuoteList from './component/QuoteList'
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <QuoteList />
 
     </div>
   );
